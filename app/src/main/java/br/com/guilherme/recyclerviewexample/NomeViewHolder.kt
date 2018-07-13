cf0f8acf61f6bfac0212c7dd2dd592497e7e6b57
administrador <administrador@M72440.local>
2018-07-13 17:40:21 -0300

    initial commit

---
+++ app/src/main/java/br/com/guilherme/recyclerviewexample/NomeViewHolder.kt
@@ -0,0 +1,14 @@
+package br.com.guilherme.recyclerviewexample
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import kotlinx.android.synthetic.main.viewholder_nome.view.*
+class NomeViewHolder(val view: View) : RecyclerView.ViewHolder(view) {
+
+    fun bind(nomeValor: String){
+        with(view){
+            nome.text = nomeValor
+        }
+    }
+
+}
